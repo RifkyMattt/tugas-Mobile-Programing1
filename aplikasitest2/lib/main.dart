a39d84bcd69b1adf5a0de8345df73b4fd60da1c1
RifkyMattt <118195048+RifkyMattt@users.noreply.github.com>
2024-11-27 14:02:33 +0700

    Update main.dart

--- aplikasitest2/lib/main.dart
+++ aplikasitest2/lib/main.dart
@@ -1,70 +1,54 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        title: 'Flutter Demo',
-        theme: ThemeData(
-          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-          useMaterial3: true,
-        ),
-        home: Biodata()
-        //const MyHomePage(title: 'Flutter Demo Home Page'),
-        );
+      title: 'Biodata',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: BiodataScreen(),
+    );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
+class BiodataScreen extends StatelessWidget {
+  final String nama = 'Rifki Ihsan Maulana';
+  final String nim = '17220218';
+  final String kelas = '17.5C.05';
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
+        title: Text('Biodata Mahasiswa'),
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: ListView(
           children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
             Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+              'Nama: $nama',
+              style: TextStyle(fontSize: 20),
             ),
+            SizedBox(height: 8),
+            Text(
+              'NIM: $nim',
+              style: TextStyle(fontSize: 20),
+            ),
+            SizedBox(height: 8),
+            Text(
+              'Kelas: $kelas',
+              style: TextStyle(fontSize: 20),
+            ),
+            // You can add more biodata entries here
           ],
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
       ),
     );
   }
